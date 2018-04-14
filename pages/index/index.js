036707b65cf0715a96446a13da4e3e9227c497e6
williamtang <tangweilin2087@gmail.com>
2018-04-15 00:34:11 +0800

    finish 4-5

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,74 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data: {
+    nowTemp: 12,
+    nowWeather: '多云',
+    nowWeatherBackground: "",
+    forecast: []
+  }, 
+  onPullDownRefresh() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad() {
+    this.getNow()
+  },
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+      data: {
+        city: 'Shanghai'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+
+        this.setData({
+          nowTemp: temp,
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
+        })
+
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+
+        let forecast = []
+        for (let i = 0; i < 24; i += 3) {
+          forecast.push({
+            time: i,
+            iconPath: '/images/sunny-icon.png',
+            temp: '12°'
+          })
+        }
+        this.setData({
+          forecast: forecast
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  }
+})
