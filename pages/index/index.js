b33b8925261755b0c908e92b8d7f0d140e21a330
williamtang <tangweilin2087@gmail.com>
2018-04-30 16:13:11 +0800

    finish 6-4

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,8 @@ const weatherColorMap = {
   'snow': '#aae1fc'
 }
 
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+
 Page({
   data: {
     nowTemp: 12,
@@ -23,7 +25,9 @@ Page({
     nowWeatherBackground: "",
     hourlyWeather: [],
     todayTemp: "",
-    todayDate: ""
+    todayDate: "",
+    city: "广州市",
+    locationTipsText: "点击获取当前位置"
   }, 
   onPullDownRefresh() {
     this.getNow(() => {
@@ -31,13 +35,16 @@ Page({
     })
   },
   onLoad() {
+    this.qqmapsdk = new QQMapWX({
+      key: 'PH5BZ-52SR6-JDKSW-ERBUF-NWBYO-I3F7Y'
+    })
     this.getNow()
   },
   getNow(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
       data: {
-        city: 'Shanghai'
+        city: this.data.city
       },
       success: res => {
         let result = res.data.result
@@ -97,6 +104,27 @@ Page({
   onTapDayWeather() {
     wx.navigateTo({
       url: '/pages/list/list',
+    })
+  },
+  onTapLocation() {
+    wx.getLocation({
+      success: res => {
+        this.qqmapsdk.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: res => {
+            let city = res.result.address_component.city
+            console.log(city)
+            this.setData({
+              city:  city,
+              locationTipsText: ""
+            })
+            this.getNow()
+          }
+        })
+      },
     })
   }
 })
