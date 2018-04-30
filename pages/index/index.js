fc306199782b822ba9881edac5ba493637f96d56
williamtang <tangweilin2087@gmail.com>
2018-04-30 15:17:07 +0800

    finish 5-5

--- pages/index/index.js
+++ pages/index/index.js
@@ -93,5 +93,10 @@ Page({
       todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
       todayDate: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} 今天`
     })
+  },
+  onTapDayWeather() {
+    wx.navigateTo({
+      url: '/pages/list/list',
+    })
   }
 })
